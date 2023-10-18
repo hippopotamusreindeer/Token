03674df7696c6577b9d33110750967038ef8cb31
Stefan Wehowsky <117749063+hippopotamusreindeer@users.noreply.github.com>
2023-10-18 12:10:06 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -11,7 +11,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
 // Define the contract, inheriting from multiple OpenZeppelin contracts for standard functionality.
 contract Token is ERC20, ERC20Burnable, Ownable, ReentrancyGuard  {
